978ecb83e2b6eb12f95ac5a7c9e8883a58b7407b
eris.adhami <eri.adhami@gmail.com>
2022-05-21 20:59:50 +0200

    Added CopyToReleaseLoc PowerShell

--- Archive.ps1
+++ Archive.ps1
@@ -2,4 +2,4 @@ $compress = @{
     Path = "/users/eriadhami/projects/AkvelonTest/MSBuild/CSharpProject2/bin/Release/net7.0/"
     DestinationPath = "/users/eriadhami/projects/AkvelonTest/MSBuild/CSharpProject2/bin/Release/net7.0/Ziped"
   }
-  Compress-Archive @compress
+Compress-Archive @compress
---
+++ CopyToReleaseLoc.ps1
@@ -0,0 +1,5 @@
+$copy = @{
+    Path = "/users/eriadhami/projects/AkvelonTest/MSBuild/CSharpProject2/bin/Release/net7.0/Ziped.zip"
+    Destination = "/users/eriadhami/projects/AkvelonTest/MSBuild/CSharpProject2/bin/Release/"
+  }
+Copy-Item @copy
